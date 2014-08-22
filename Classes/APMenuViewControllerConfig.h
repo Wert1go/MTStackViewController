efd231b542cb93bed090ecc1d60abc4ce1ca183b
KazaiMazai <kazaimazai@gmail.com>
2014-08-22 13:00:21 +0400

    colors and fonts config

--- Classes/APMenuViewControllerConfig.h
+++ Classes/APMenuViewControllerConfig.h
@@ -9,32 +9,32 @@
 #import <Foundation/Foundation.h>
 
 // Menu Controller
-#define kBackground				[UIColor colorWithRed:45.0f/255.0f green:45.0f/255.0f blue:45.0f/255.0f alpha:1.0]
-#define kSelectionBackground	[UIColor colorWithRed:15.0f/255.0f green:15.0f/255.0f blue:15.0f/255.0f alpha:1.0]
+#define kBackground             [UIColor clearColor]	//[UIColor colorWithRed:45.0f/255.0f green:45.0f/255.0f blue:45.0f/255.0f alpha:1.0]
+#define kSelectionBackground	[UIColor colorWithRed: 171.0f/255.0f green:171.0f/255.0f blue:171.0f/255.0f alpha:1.0]
 
 // Table Cell
 #define kImagePadding		50
 #define kTextPadding		20
 #define kBadgePosition		220
 #define kCellBackground		[UIColor colorWithRed:45.0f/255.0f green:45.0f/255.0f blue:45.0f/255.0f alpha:1.0].CGColor
-#define kUpperSeparator		[UIColor colorWithRed:18.0f/255.0f green:18.0f/255.0f blue:20.0f/255.0f alpha:1.0].CGColor
-#define kLowerSeparator		[UIColor colorWithRed:66.0f/255.0f green:66.0f/255.0f blue:66.0f/255.0f alpha:1.0].CGColor
-#define kCellFontColor		[UIColor colorWithRed:180.0f/255.0f green:180.0f/255.0f blue:180.0f/255.0f alpha:1.0f]
-#define kCellShadowColor	[UIColor colorWithRed:0.21 green:0.15 blue:0.19 alpha:1.0]
+#define kUpperSeparator		[UIColor clearColor].CGColor
+#define kLowerSeparator		[UIColor clearColor].CGColor
+#define kCellFontColor		[UIColor blackColor];
+#define kCellShadowColor	[UIColor clearColor]
 
 // Header Cell
 #define	kTextHeaderPadding	10
 #define kGradientUp			[UIColor colorWithRed:31.0f/255.0f green:32.0f/255.0f blue:33.0f/255.0f alpha:1.0]
 #define kGradientDown		[UIColor colorWithRed:31.0f/255.0f green:32.0f/255.0f blue:33.0f/255.0f alpha:1.0]
-#define kUpperSeparator		[UIColor colorWithRed:18.0f/255.0f green:18.0f/255.0f blue:20.0f/255.0f alpha:1.0].CGColor
-#define kLowerSeparator		[UIColor colorWithRed:66.0f/255.0f green:66.0f/255.0f blue:66.0f/255.0f alpha:1.0].CGColor
-#define kFontColor			[UIColor colorWithRed:0.49 green:0.50 blue:0.57 alpha:1.0]
-#define kFontShadowColor	[UIColor colorWithRed:0.21 green:0.15 blue:0.19 alpha:1.0]
+//#define kUpperSeparator		[UIColor blackColor]   //[UIColor colorWithRed:18.0f/255.0f green:18.0f/255.0f blue:20.0f/255.0f alpha:1.0].CGColor
+//#define kLowerSeparator		[UIColor colorWithRed:66.0f/255.0f green:66.0f/255.0f blue:66.0f/255.0f alpha:1.0].CGColor
+#define kFontColor			[UIColor blackColor]
+#define kFontShadowColor	[UIColor clearColor]
 
-#define kCommonFontName @"HelveticaNeue"
-#define kCommonFontNameBold @"HelveticaNeue-Bold"
+#define kCommonFontName @"OpenSans-Light"
+#define kCommonFontNameBold @"OpenSans-Light"
 
-#define kCommonFontSize 18.0f
+#define kCommonFontSize 16.0f
 
 @interface APMenuViewControllerConfig : NSObject
 
